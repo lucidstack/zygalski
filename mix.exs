250b4e5453ae8cddd47f3d28cb24faff08a78a90
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-08-01 01:07:57 +0200

    start Zygalski app at startup

--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Zygalski.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :plug, :cowboy]]
+    [
+      applications: [:logger, :plug, :cowboy],
+      mod:          {Zygalski, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
