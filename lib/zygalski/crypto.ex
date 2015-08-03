f4e94fa4550dfc6c7f813f2fc0787e3226e1f9e5
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-08-04 00:06:29 +0200

    encode64 and decode64 messages when encrypting/decrypting

--- lib/zygalski/crypto.ex
+++ lib/zygalski/crypto.ex
@@ -3,16 +3,16 @@ defmodule Zygalski.Crypto do
   alias Zygalski.SslUtils
 
   def encrypt(message, key_name, public_key \\ :public_key, file \\ File, key \\ Key) do
-    key_content = key_content(key_name, file, :public)
-    message |> public_key.encrypt_public(key.decode(key_content))
+    key = key_name |> key_content(:public, file) |> key.decode
+    message |> public_key.encrypt_public(key) |> Base.encode64
   end
 
   def decrypt(cipher_text, key_name, password, public_key \\ :public_key, file \\ File, key \\ Key) do
-    key_content = key_content(key_name, file, :private)
-    cipher_text |> public_key.decrypt_private(key.decode_with_password(key_content, password))
+    key = key_name |> key_content(:private, file) |> key.decode_with_password(password)
+    cipher_text |> Base.decode64 |> public_key.decrypt_private(key)
   end
 
-  defp key_content(key_name, file, type) do
+  defp key_content(key_name, type, file) do
     {:ok, key_content} = SslUtils.key_path(key_name, type) |> file.read
     key_content
   end
