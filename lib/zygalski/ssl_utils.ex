7d4df4f2a96adb2efbc39e89e5c83b6d2336fb37
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-08-03 23:06:13 +0200

    add call to openssl for public key generation

--- lib/zygalski/ssl_utils.ex
+++ lib/zygalski/ssl_utils.ex
@@ -1,7 +1,7 @@
 defmodule Zygalski.SslUtils do
   def create_pair(key_name, passphrase, system \\ System) do
     system.cmd("openssl", private_key_args(key_name, passphrase))
-    #system.cmd("openssl", public_key_args(key_name, passphrase))
+    system.cmd("openssl", public_key_args(key_name, passphrase))
   end
 
   defp private_key_args(key_name, passphrase),
@@ -10,6 +10,13 @@ defmodule Zygalski.SslUtils do
     "-#{key_type}", "-passout", "pass:#{passphrase}",
     "-out", key_path(key_name, :private),
     2048
+  ]
+
+  defp public_key_args(key_name, passphrase),
+  do: [
+    "rsa",
+    "-in", key_path(key_name, :private), "-passin", "pass:#{passphrase}",
+    "-pubout", "-out", key_path(key_name, :public)
   ]
 
   defp key_type,
