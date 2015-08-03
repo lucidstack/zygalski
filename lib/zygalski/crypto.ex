48e58e4a62f90514505fd7efa8afce4f83286518
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-08-04 00:14:21 +0200

    use meck for Zygalski.Crypto tests

--- lib/zygalski/crypto.ex
+++ lib/zygalski/crypto.ex
@@ -2,18 +2,18 @@ defmodule Zygalski.Crypto do
   alias Zygalski.Key
   alias Zygalski.SslUtils
 
-  def encrypt(message, key_name, public_key \\ :public_key, file \\ File, key \\ Key) do
-    key = key_name |> key_content(:public, file) |> key.decode
-    message |> public_key.encrypt_public(key) |> Base.encode64
+  def encrypt(message, key_name) do
+    key = key_name |> key_content(:public) |> Key.decode
+    message |> :public_key.encrypt_public(key) |> Base.encode64
   end
 
-  def decrypt(cipher_text, key_name, password, public_key \\ :public_key, file \\ File, key \\ Key) do
-    key = key_name |> key_content(:private, file) |> key.decode_with_password(password)
-    cipher_text |> Base.decode64 |> public_key.decrypt_private(key)
+  def decrypt(cipher_text, key_name, password) do
+    key = key_name |> key_content(:private) |> Key.decode_with_password(password)
+    cipher_text |> Base.decode64 |> :public_key.decrypt_private(key)
   end
 
-  defp key_content(key_name, type, file) do
-    {:ok, key_content} = SslUtils.key_path(key_name, type) |> file.read
+  defp key_content(key_name, type) do
+    {:ok, key_content} = SslUtils.key_path(key_name, type) |> File.read
     key_content
   end
 end
--- test/zygalski/crypto_test.exs
+++ test/zygalski/crypto_test.exs
@@ -1,36 +1,27 @@
 defmodule ZygalskiCryptoTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   import Zygalski.Crypto
 
+  setup_all do
+    :meck.new(File)
+    :meck.new(:public_key)
+    :meck.new(Zygalski.Key)
+    on_exit fn -> :meck.unload end
+  end
+
   test "#encrypt, given a message and a key_name, returns the encrypted message" do
-    defmodule FileMock,
-    do: (def read(_), do: {:ok, "thekeycontent"})
+    :meck.expect(File,         :read,           fn(_) -> {:ok, "thekeycontent"} end)
+    :meck.expect(:public_key,  :encrypt_public, fn(_,_) -> "3ncrypt3d m3ss4g3" end)
+    :meck.expect(Zygalski.Key, :decode,         fn(_) -> true end)
 
-    defmodule PublicKeyMock,
-    do: (def encrypt_public(_,_), do: "3ncrypt3d m3ss4g3")
-
-    defmodule KeyMock,
-    do: (def decode(_), do: true)
-
-    assert encrypt(
-      "the message", "the_key",
-      PublicKeyMock, FileMock, KeyMock
-    ) == "3ncrypt3d m3ss4g3"
+    assert encrypt("the message", "the_key") == "M25jcnlwdDNkIG0zc3M0ZzM="
   end
 
   test "#decrypt, given a message, a passphrase, and a key_name, returns the decrypted message" do
-    defmodule FileMock,
-    do: (def read(_), do: {:ok, "thekeycontent"})
+    :meck.expect(File,         :read,                 fn(_) -> {:ok, "thekeycontent"} end)
+    :meck.expect(:public_key,  :decrypt_private,      fn(_,_) -> "decrypted message" end)
+    :meck.expect(Zygalski.Key, :decode_with_password, fn(_,_) -> true end)
 
-    defmodule PublicKeyMock,
-    do: (def decrypt_private(_,_), do: "decrypted message")
-
-    defmodule KeyMock,
-    do: (def decode_with_password(_,_), do: true)
-
-    assert decrypt(
-      "3ncrypt3d m3ss4g3", "the_key", "password",
-      PublicKeyMock, FileMock, KeyMock
-    ) == "decrypted message"
+    assert decrypt("3ncrypt3d m3ss4g3", "the_key", "password") == "decrypted message"
   end
 end
