a340ed4ef8e8d3d23073d155956db23939f7d427
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-08-04 17:38:11 +0200

    add /encrypt and /decrypt actions

--- lib/zygalski/router.ex
+++ lib/zygalski/router.ex
@@ -18,7 +18,33 @@ defmodule Zygalski.Router do
     |> send_resp(200, "Key created/updated!")
   end
 
+  post "/encrypt" do
+    key_name = conn.params["channel_name"]
+    message = conn.params["text"]
+    encrypted_message = Crypto.encrypt(message, key_name)
+
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, encrypted_message)
+  end
+
+  post "/decrypt" do
+    key_name = conn.params["channel_name"]
+    {passphrase, message} = conn.params["text"] |> extract_decrypt_text
+
+    original_message = Crypto.decrypt(message, passphrase, key_name)
+
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, original_message)
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
+  end
+
+  defp extract_decrypt_text(text) do
+    {passphrase, [message]} = text |> String.split |> Enum.split(-1)
+    {passphrase |> Enum.join(" "), message}
   end
 end
--- test/zygalski/router_test.exs
+++ test/zygalski/router_test.exs
@@ -4,6 +4,7 @@ defmodule ZygalskiRouterTest do
 
   setup_all do
     :meck.new(Zygalski.SslUtils)
+    :meck.new(Zygalski.Crypto)
     on_exit fn -> :meck.unload end
   end
 
@@ -21,5 +22,31 @@ defmodule ZygalskiRouterTest do
     assert conn.state == :sent
     assert conn.status == 200
     assert conn.resp_body == "Key created/updated!"
+  end
+
+  test "POST /encrypt return the encrypted message" do
+    :meck.expect(Zygalski.Crypto, :encrypt, fn("the passphrase", "test") -> "3ncrypt3d" end)
+
+    conn = conn(:post, "/encrypt", "channel_name=test&text=the passphrase")
+      |> put_req_header("content-type", "application/x-www-form-urlencoded")
+      |> Zygalski.Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "3ncrypt3d"
+  end
+
+  test "POST /decrypt return the decrypted message" do
+    :meck.expect(Zygalski.Crypto, :decrypt, fn("3ncrypt3d", "the passphrase", "test") ->
+      "decrypted"
+    end)
+
+    conn = conn(:post, "/decrypt", "channel_name=test&text=the passphrase 3ncrypt3d")
+      |> put_req_header("content-type", "application/x-www-form-urlencoded")
+      |> Zygalski.Router.call(@opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "decrypted"
   end
 end
